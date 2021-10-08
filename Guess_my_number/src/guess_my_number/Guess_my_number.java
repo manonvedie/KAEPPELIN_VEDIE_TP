4a2ab48777e7be98870901f1c4b391efa99cbb23
KAEPPELIN Mayeul <KAEPPELIN Mayeul@192.168.115.191>
2021-10-08 17:18:13 +0200

    TP1_exo3

---
+++ Guess_my_number/src/guess_my_number/Guess_my_number.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guess_my_number;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author KAEPPELIN Mayeul
+ */
+public class Guess_my_number {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner sc;
+        sc = new Scanner(System.in);
+        
+        String entrée = "";
+        System.out.println("choisisez votre niveau; facile, normal ou difficile");
+        entrée = sc.next();
+        if ("facile".equals(entrée) ){
+            System.out.println("aloooooooooooooo");
+            System.out.println("entrez un nombre entre 1 et 100");
+            
+            double nb = sc.nextDouble();
+            int compteur = 0;
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(100);
+            System.out.println(n);
+            
+            while (nb != n){
+                if(nb>n){
+                    System.out.println("trop grand");
+                    nb = sc.nextDouble();
+                    compteur +=1;
+
+                }
+                if(nb<n){
+                    System.out.println("trop petit");
+                    nb = sc.nextDouble();
+                    compteur +=1;
+
+
+                }
+                if(nb==n){
+                    System.out.println("bien joué");
+
+                    compteur +=1;
+                    System.out.println("vous avez mis " + compteur + " essais");
+                
+
+
+       
+                    
+        if ("difficile".equals(entrée) ){
+            System.out.println("alo coco");
+            System.out.println("entrez un nombre entre 1 et 1000");
+            
+            double nb1 = sc.nextDouble();
+            int compteur1 = 0;
+            int n1 = generateurAleat.nextInt(1000);
+            System.out.println(n1);
+            
+            while (nb1 != n1){
+                if(nb1>n1){
+                    System.out.println("trop grand");
+                    nb1 = sc.nextDouble();
+                    compteur1 +=1;
+
+                }
+                if(nb<n){
+                    System.out.println("trop petit");
+                    nb = sc.nextDouble();
+                    compteur +=1;
+
+
+                }
+                if(nb==n){
+                    System.out.println("bien joué");
+
+                    compteur +=1;
+                    System.out.println("vous avez mis " + compteur + " essais");
+                
+
+
+       
+
+
+            }
+        }
+    }
+
+            }
+        }
+    }
+            
+        }
+        
+
+         
+
+
+        
+        
+        
+}
+        
+
---
+++ Guess_my_number/src/guess_my_number/generateurAleat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guess_my_number;
+
+/**
+ *
+ * @author KAEPPELIN Mayeul
+ */
+class generateurAleat {
+
+    static int nextInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
