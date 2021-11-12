3fb068c218edadbbdbb8de2fa7050eb7c281f487
vedie <vedie@192.168.114.146>
2021-11-12 17:14:11 +0100

    Version1

---
+++ SuperPuissance4_Kaeppelin_Védie/src/Joueur.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vedie
+ */
+public class Joueur {
+    String Nom;
+    String Couleur;
+    int [] ListeJetons = new int [21];
+    int nombreJetonsRestants;
+    int nombreDesintegrateurs;
+    
+    sc Scanner;
+    
+}
