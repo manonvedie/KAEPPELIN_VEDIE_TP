974564b465ee4254d4f0d5bb1e4207a7254e8924
KAEPPELIN Mayeul <KAEPPELIN Mayeul@192.168.115.191>
2021-11-12 18:22:09 +0100

    leur blague c'est notre blague ils sont nuls j'ai fait la classe cellule

---
+++ SuperPuissance4_Kaeppelin_Védie/src/Cellule.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author KAEPPELIN Mayeul
+ */
+public class Cellule {
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
+    
+    public void Cellule(){
+        jetonCourant=null;
+        trouNoir = true;
+        desintegrateur = true;
+    }    
+    public boolean affecterJeton(Jeton JETON){
+        if (jetonCourant== null) {
+            jetonCourant = JETON;
+            return true;            
+        }
+        else {
+            return false;
+        }
+            
+        
+    public lireCouleurDuJeton () {
+        JETON 
+        
+        
+    }
+            
+    }
+    
+    
+}
