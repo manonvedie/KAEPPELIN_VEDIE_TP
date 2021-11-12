1ba957ec9d4ad1db6b2433005c68396cab7196cd
vedie <vedie@192.168.114.146>
2021-11-12 19:01:12 +0100

    grille class v2

--- SuperPuissance4_Kaeppelin_Védie/src/Grille.java
+++ SuperPuissance4_Kaeppelin_Védie/src/Grille.java
@@ -7,6 +7,25 @@
  *
  * @author vedie
  */
+
 public class Grille {
-  Cellule CellulesJeu [][] = new Cellule [6][7]; 
+  Cellule CellulesJeu [][];
+  
+  public Grille() {
+      CellulesJeu= new Cellule [6][7];
+      for (int i=0; i<6; i++){
+          for (int j=0; j<7; i++) {
+              CellulesJeu[i][j] = new Cellule();
+          }
+      }
+      
+  }
+  public boolean ajouterJetonDansColonne(Jeton J, int Colonne){
+     // on va dans la colonne choisie, on parcourt les lignes du bas vers le haut jusqu'a qu'il y en ait une vide --> on met le jeton dedans.
+    for (int i=5; i>0; i--) {
+        if(CellulesJeu[i][Colonne].jetonCourant==null){
+            J=CellulesJeu[i][Colonne].jetonCourant;
+        }
+    } 
+}
 }
