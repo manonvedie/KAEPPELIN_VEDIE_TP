ca11f41790f9d460647fe94c87b5ef92bff05f42
vedie <vedie@192.168.114.146>
2021-11-15 12:00:55 +0100

    deplacement des fichiers dans le bon package

--- SuperPuissance4_Kaeppelin_Védie/src/superpuissance4_kaeppelin_védie/SuperPuissance4_Kaeppelin_Védie.java
+++ SuperPuissance4_Kaeppelin_Védie/src/superpuissance4_kaeppelin_védie/SuperPuissance4_Kaeppelin_Védie.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package superpuissance4_kaeppelin_védie;
-import Joueur;
 
 /**
  *
@@ -18,6 +17,7 @@ public class SuperPuissance4_Kaeppelin_Védie {
         // TODO code application logic here
         Jeton j= new Jeton("rouge");
         Grille G = new Grille ();
+        G.afficherGrilleSurConsole();
         
     }
     
