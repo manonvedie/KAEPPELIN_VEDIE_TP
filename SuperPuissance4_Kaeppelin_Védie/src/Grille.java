da5cbf25c690dca89933f3d3ed87ae44076ba0ad
vedie <vedie@192.168.114.146>
2021-11-15 11:41:59 +0100

    grille class v3

--- SuperPuissance4_Kaeppelin_Védie/src/Grille.java
+++ SuperPuissance4_Kaeppelin_Védie/src/Grille.java
@@ -20,12 +20,66 @@ public class Grille {
       }
       
   }
-  public boolean ajouterJetonDansColonne(Jeton J, int Colonne){
+  public boolean ajouterJetonDansColonne(Jeton J, int Colonne){ // fonction validée par le prof
      // on va dans la colonne choisie, on parcourt les lignes du bas vers le haut jusqu'a qu'il y en ait une vide --> on met le jeton dedans.
-    for (int i=5; i>0; i--) {
+    if (CellulesJeu[0][Colonne].jetonCourant!=null) {
+            return false;
+    }
+     
+    for (int i=5; i>=0; i--) { 
         if(CellulesJeu[i][Colonne].jetonCourant==null){
-            J=CellulesJeu[i][Colonne].jetonCourant;
+            return CellulesJeu[i][Colonne].affecterJeton(J);
+           
         }
-    } 
+    
 }
+    return true;
 }
+  
+  public boolean etreRemplie(){
+      for (int i=0; i<6; i++) {
+          for (int j=0; j<7; i++) {
+              if(CellulesJeu[i][j]== null){
+                  return true;    
+              }
+              else {
+                  return false;
+              }
+          }
+      }
+      return true;
+  }
+
+  public void viderGrille() {
+      for (int i=0; i<6; i++) {
+          for (int j=0; j<7; i++) {
+             CellulesJeu[i][j].jetonCourant=null; 
+             CellulesJeu[i][j].trouNoir=false;
+             CellulesJeu[i][j].desintegrateur=false;
+          }
+      }
+  }
+  
+  public void afficherGrilleSurConsole() {
+      for(int i=0; i<6; i++) {
+          for (int j=0; j<7; j++) {
+              if (CellulesJeu[i][j].lireCouleurDuJeton()=="rouge") {
+                  System.out.println("R");
+              }
+              if (CellulesJeu[i][j].lireCouleurDuJeton()=="jaune") {
+                  System.out.println("J");
+              }
+              if (CellulesJeu[i][j].lireCouleurDuJeton()=="vide") {
+                  System.out.println("/");
+              }
+          }
+      }
+  }
+  
+  public boolean celluleOccupee(int ligne, int col) {
+      return true;
+  }
+}
+
+  
+
