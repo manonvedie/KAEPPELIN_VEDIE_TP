5e255469a607599c73f4593e5ad2c656cb00f8ad
KAEPPELIN Mayeul <KAEPPELIN Mayeul@192.168.115.191>
2021-12-03 14:59:25 +0100

    création du projet

--- Mastemind/src/mastemind/Mastemind.java
+++ Mastemind/src/mastemind/Mastemind.java
@@ -14,7 +14,6 @@ public class Mastemind {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
